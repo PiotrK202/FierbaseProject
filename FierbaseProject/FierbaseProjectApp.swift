718cc59e35ac186a01abe5362f0ce1ba6aa27f33
PiotrK <piotr.koscielny69@gmail.com>
2025-09-01 09:45:29 +0200

    Initial Commit

---
+++ FierbaseProject/FierbaseProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FierbaseProjectApp.swift
+//  FierbaseProject
+//
+//  Created by piotr koscielny on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct FierbaseProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
