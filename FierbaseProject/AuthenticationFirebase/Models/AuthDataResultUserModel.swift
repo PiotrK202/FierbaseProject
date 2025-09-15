93ceecede41231d94955a32d235149e46860ca4b
PiotrK <piotr.koscielny69@gmail.com>
2025-09-15 14:21:27 +0200

    small change in auth data model

--- FierbaseProject/AuthenticationFirebase/Models/AuthDataResultUserModel.swift
+++ FierbaseProject/AuthenticationFirebase/Models/AuthDataResultUserModel.swift
@@ -12,16 +12,19 @@ struct AuthDataResultUserModel {
     let uid: String
     let email: String?
     let photoURL: String?
+    let isAnonymous: Bool?
     
     init(user: User) {
         self.uid = user.uid
         self.email = user.email
         self.photoURL = user.photoURL?.absoluteString
+        self.isAnonymous = user.isAnonymous
     }
     
-    init(uid: String, email: String, photoURL: String?) {
+    init(uid: String, email: String, photoURL: String?, isAnonymous: Bool = false) {
         self.uid = uid
         self.email = email
         self.photoURL = photoURL
+        self.isAnonymous = isAnonymous
     }
 }
